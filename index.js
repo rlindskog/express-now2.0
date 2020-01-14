712a252162b84b1b661d551fbadcce4f1276c50a
Ryan Lindskog <r.lindskog17@gmail.com>
2020-01-14 13:40:36 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' })
+})
+
+app.get('/api', (req, res) => {
+  res.json({ api: 'world' })
+})
+
+app.listen(process.env.PORT || 3000)
